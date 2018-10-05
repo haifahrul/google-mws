41ea06496205b215491477ffc2870c6310c02422
A Fakhrurozi S <haifahrul@gmail.com>
2018-10-05 14:25:58 +0700

    fix redesign menu calculator - project1

--- public/project1/js/project1.js
+++ public/project1/js/project1.js
@@ -1,16 +1,24 @@
-function add2numbers() {
-  let input = document.querySelectorAll("input");
-  let value1 = input[0].value;
-  let value2 = input[1].value;
-  let result;
-
-  result = parseFloat(value1) + parseFloat(value2);
-
-  if (!isNaN(result) && result !== "") {
-    input[2].value = result;
+function calculate() {
+  if (this.name === "input1") {
+    input1 = this.value;
+  }
+  if (this.name === "input2") {
+    input2 = this.value;
+  }
+  if (input1 && input2) {
+    result = parseFloat(input1) + parseFloat(input2);
+    document.getElementById("result").innerHTML = result;
+  } else {
+    document.getElementById("result").innerHTML = "";
   }
 }
 
-let button = document.querySelector("button");
+let result;
+let input1;
+let input2;
+let i;
+const allInput = document.getElementsByTagName("input");
 
-button.addEventListener("click", add2numbers);
+for (i = 0; i < allInput.length; i++) {
+  allInput[i].addEventListener("keyup", calculate, false);
+}
