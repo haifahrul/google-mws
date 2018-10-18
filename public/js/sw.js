809b65b6c0c90082f3d569c4a7addbac258f1ed2
A Fakhrurozi S <haifahrul@gmail.com>
2018-10-19 00:53:16 +0700

    service worker

---
+++ public/js/sw.js
@@ -0,0 +1,51 @@
+const staticCachesName = 'static-v10'
+const fileToCaches = [
+  '/',
+  '/index.html',
+  '/header.html',
+  '/header-back.html',
+  '/footer.html',
+  '/404.html',
+  '/manifest.json',
+  '/css/main.css',
+  '/js/app.min.js',
+  '/images/avatar.png',
+  '/images/logo.png',
+  '/project1/css/project1.css',
+  '/project1/index.html',
+  '/project2/css/project1.css',
+  '/project2/index.html'
+];
+
+// Install SW
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(staticCachesName).then((cache) => {
+      return cache.addAll(fileToCaches)
+    })
+  )
+})
+
+self.addEventListener('activate', () => {
+  console.log('Active')
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if(response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          return caches.open(staticCachesName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        });
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  )
+})
